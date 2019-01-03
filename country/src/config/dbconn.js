944bdb3fc160a1421a8757dec4cb9ce4844455f8
lee <33998600+leeping88901024@users.noreply.github.com>
2019-01-03 15:48:33 +0800

    Delete dbconn.js

--- country/src/config/dbconn.js
+++
@@ -1,9 +0,0 @@
-const dbConn = {
-	user          : "F2WEBSHANDONGUP",
-    password      : "7V1i436483BuS1BGe5T2Q4fY",
-    connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.0.199)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=nbsss)))"
-}
-
-module.exports = {
-    dbConn,
-}
