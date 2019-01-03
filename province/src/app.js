3527b70c391a34d14669e41ed8568a3428bb6fe1
lee <leeping88901024@gmail.com>
2019-01-03 16:16:38 +0800

    Update app.js

--- province/src/app.js
+++ province/src/app.js
@@ -25,7 +25,8 @@ var app = async (baseUrl) => {
         ret:  { dir: oracledb.BIND_OUT, type: oracledb.CURSOR }
     }
     sql = `BEGIN :ret := f2webshangdong.getTBiBldIssueSummary(:date_from, :date_to); END;`
-    await upload(sql, OrgId, TBiBldIssueSummary, '20563ef7e39645a984fac3b799282ec5', '10', `${baseUrl}/uploaddataserv`, bindPara, {}, 1000)
+    ret = await upload(sql, OrgId, TBiBldIssueSummary, '20563ef7e39645a984fac3b799282ec5', '10', `${baseUrl}/uploaddataserv`, bindPara, {}, 1000)
+    console.log(`hh${ret}`)
     sql = `BEGIN :ret := f2webshangdong.getTBiBldIssuing(:date_from, :date_to); END;`
     await upload(sql, OrgId, TBiBldIssuing, '20563ef7e39645a984fac3b799282ec5', '10', `${baseUrl}/uploaddataserv`, bindPara, {}, 1000);
 }
