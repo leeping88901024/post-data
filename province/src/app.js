5f230d1616199674b1d700b0a2b2133541e57f64
lee <leeping88901024@gmail.com>
2019-01-03 16:59:57 +0800

    success?

--- province/src/app.js
+++ province/src/app.js
@@ -26,10 +26,11 @@ var app = async (baseUrl) => {
         ret:  { dir: oracledb.BIND_OUT, type: oracledb.CURSOR }
     }
     sql = `BEGIN :ret := f2webshangdong.getTBiBldIssueSummary(:date_from, :date_to); END;`
-    ret = await upload(sql, OrgId, TBiBldIssueSummary, '20563ef7e39645a984fac3b799282ec5', '10', `${baseUrl}/uploaddataserv`, bindPara, {}, 1000)
-    console.log(`hh${ret}`)
+    var ret1 = await upload(sql, OrgId, TBiBldIssueSummary, '20563ef7e39645a984fac3b799282ec5', '10', `${baseUrl}/uploaddataserv`, bindPara, {}, 1000)
+    console.log(`eeeeee${ret1}`)
     sql = `BEGIN :ret := f2webshangdong.getTBiBldIssuing(:date_from, :date_to); END;`
-    await upload(sql, OrgId, TBiBldIssuing, '20563ef7e39645a984fac3b799282ec5', '10', `${baseUrl}/uploaddataserv`, bindPara, {}, 1000);
+    ret2 = await upload(sql, OrgId, TBiBldIssuing, '20563ef7e39645a984fac3b799282ec5', '10', `${baseUrl}/uploaddataserv`, bindPara, {}, 1000);
+    console.log(`ooooo${ret2}`)
 }
 
 app(baseUrl);
